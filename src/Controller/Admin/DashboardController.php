7f60f71f142b1b4875c97a8755b48f6ac437ca7e
chabrackgit <isndigital22@gmail.com>
2023-04-13 22:06:27 +0200

    gestion des liaisons import/export

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,7 +9,7 @@ use App\Entity\Session;
 use App\Entity\TemplateCode;
 use App\Entity\TypeTransfert;
 use App\Repository\FichierRepository;
-use App\Repository\SessionRepository;
+use App\Repository\TemplateCodeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -21,32 +21,40 @@ class DashboardController extends AbstractDashboardController
 {
 
     private $fichierRepository;
-    private $sessionRepository;
+    private $templateCodeRepository;
 
 
     public function __construct(
         FichierRepository $fichierRepository,
-        SessionRepository $sessionRepository
+        TemplateCodeRepository $templateCodeRepository
         )
     {
         $this->fichierRepository = $fichierRepository;
-        $this->sessionRepository = $sessionRepository;
+        $this->templateCodeRepository = $templateCodeRepository;
     }
 
     #[Route('/admin3fza38fv', name: 'admin')]
     #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
-        $sessionExport = $this->sessionRepository->findOneBy(['code' => Fichier::SESSION_EXPORT]);
-        $sessionReport = $this->sessionRepository->findOneBy(['code' => Fichier::SESSION_REPORT]);
-        $fichiersExport = $this->fichierRepository->findBy(['session' => $sessionExport]);
-        $fichiersReport = $this->fichierRepository->findBy(['session' => $sessionReport]);
-        $countTotal = count($fichiersExport) + count($fichiersReport);
+        $sessionExportUbw = $this->templateCodeRepository->findOneBy(['code' => 'RDCV']);
+        $sessionExportPs = $this->templateCodeRepository->findOneBy(['code' => 'PS']);
+        $sessionImportUbw = $this->templateCodeRepository->findOneBy(['code' => 'SDD']);
+        $sessionImportPs = $this->templateCodeRepository->findOneBy(['code' => 'PS_SCT']);
+        $fichiersExportUbw = $this->fichierRepository->findBy(['templateCode' => $sessionExportUbw]);
+        $fichiersExportPs = $this->fichierRepository->findBy(['templateCode' => $sessionExportPs]);
+        $countTotalExport = count($fichiersExportUbw) + count($fichiersExportPs);
+        $fichiersImportUbw = $this->fichierRepository->findBy(['templateCode' => $sessionImportUbw]);
+        $fichiersImportPs = $this->fichierRepository->findBy(['templateCode' => $sessionImportPs]);
+        $countTotalImport = count($fichiersImportUbw) + count($fichiersImportPs);
         //return $this->render('bundles/EasyAdminBundle/welcome.html.twig');
         return $this->render('admin/dashboard.html.twig', [
-            'countTotal' => $countTotal,
-            'countExport' => count($fichiersExport),
-            'countReport' => count($fichiersReport),
+            'countTotalExport' => $countTotalExport,
+            'countTotalImport' => $countTotalImport,
+            'countExportUbw' => count($fichiersExportUbw),
+            'countExportPs' => count($fichiersExportPs),
+            'countImportUbw' => count($fichiersImportUbw),
+            'countImportPs' => count($fichiersImportPs),
         ]);
     }
 
@@ -60,7 +68,7 @@ class DashboardController extends AbstractDashboardController
     {
         if ($this->isGranted('ROLE_TECHNICIEN') || $this->isGranted('ROLE_ADMIN')) {
             yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-            yield MenuItem::linkToCrud('Transfert Export', 'fas fa-arrow-down', Fichier::class);
+            yield MenuItem::linkToCrud('Liste des transferts', 'fas fa-arrow-right-arrow-left', Fichier::class);
             yield MenuItem::linkToCrud('Entite', 'fas fa-school', Entite::class);
             yield MenuItem::linkToCrud('Template code', 'fas fa-money-bill-trend-up', TemplateCode::class);
             yield MenuItem::linkToCrud('Type (IMPORT/EXPORT)', 'fas fa-solid fa-money-bill-transfer', TypeTransfert::class);
